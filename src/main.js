9b1bc0280c068e6cd5a5874b84f84abc96a297f1
caicai0821 <736814997@qq.com>
2023-04-19 16:51:17 +0800

    基本配置

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
+/*
+ * @Author       : 蔡诗涵
+ * @Date         : 2023-04-19 11:18:08
+ * @LastEditTime : 2023-04-19 16:40:34
+ * @Description  :
+ *
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios' // 引入axios
+import '@/assets/index.less'
 
 Vue.prototype.$axios = axios // 把axios挂载到vue上
 
