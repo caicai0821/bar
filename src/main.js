d01fea67fdbdb1a3fb526f86705e1dc76be21d77
caicai0821 <736814997@qq.com>
2023-04-19 11:26:06 +0800

    初始化配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios' // 引入axios
 
+Vue.prototype.$axios = axios // 把axios挂载到vue上
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
